f54a9872aeccd46163c0e99c92c37d99c85dddc9
taranu <dan.s.taranu@gmail.com>
2022-11-04 14:13:19 -0400

    Add python/lib tests

--- python/lib/tests/test_pyimage.cc
+++ python/lib/tests/test_pyimage.cc
@@ -4,40 +4,44 @@
 
 #include <memory>
 
-#include "image.h"
-#include "vectorimage.h"
+#include "gauss2d/image.h"
+#include "pyimage.h"
+
+#include <pybind11/embed.h>
 
 namespace g2 = gauss2d;
 
-typedef g2::VectorImage<double> Image; 
+typedef g2::python::PyImage<double> Image; 
 typedef g2::ImageArray<double, Image> ImageArray; 
-typedef g2::VectorImage<bool> Mask;
+typedef g2::python::PyImage<bool> Mask;
 
-TEST_CASE("VectorImage")
+// Import numpy, otherwise all Image calls will segfault
+py::scoped_interpreter guard{};
+py::module_ numpy = py::module_::import("numpy");
+
+TEST_CASE("PyImage")
 {
     size_t n_rows = 3, n_cols = 2;
     std::shared_ptr<Image> image = std::make_shared<Image>(n_rows, n_cols);
     Image zeros{n_rows, n_cols};
     auto x = Image(n_rows, n_cols) == zeros;
-
     CHECK(x);
-
     CHECK(image->get_coordsys() == g2::COORDS_DEFAULT);
     CHECK(&(image->get_coordsys()) == &(g2::COORDS_DEFAULT));
-
     CHECK(image->get_n_cols() == n_cols);
     CHECK(image->get_n_rows() == n_rows);
     CHECK(image->size() == n_cols*n_rows);
 
+    image->set_value_unchecked(0, 0, 0);
+    CHECK(image->get_value_unchecked(0, 0) == 0);
     image->add_value_unchecked(0, 0, 1);
-    CHECK(*image != zeros);
     CHECK(image->get_value_unchecked(0, 0) == 1);
+    CHECK(*image != zeros);
     CHECK_THROWS_AS(image->get_value(n_rows, n_cols), std::out_of_range);
     CHECK_THROWS_AS(image->set_value(n_rows, n_cols, 1), std::out_of_range);
     double & value = image->_get_value_unchecked(1, 1);
     value = -1;
     CHECK(image->get_value_unchecked(1, 1) == -1);
-
 }
 
 TEST_CASE("VectorImageArray")
@@ -54,7 +58,6 @@ TEST_CASE("VectorImageArray")
         CHECK(img == image);
     }
 }
-
 
 TEST_CASE("VectorMask")
 {
