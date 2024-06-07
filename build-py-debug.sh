23e142d9446ea227dc90cf47a5b3b10a4ed762c0
Dan Taranu <dan.s.taranu@gmail.com>
2024-06-07 18:30:29 -0400

    Update build scripts to allow debug lib installation

--- build-py-debug.sh
+++ build-py-debug.sh
@@ -1,4 +1,3 @@
 # Compile python debug libraries
-meson compile -C python/lib/build-debug
-meson compile -C python/build-debug
-
+meson compile -C python/lib/build-debug && meson install -C python/lib/build-debug && meson test -C python/lib/build-debug
+meson compile -C python/build-debug && meson install -C python/build-debug && meson test -C python/build-debug
